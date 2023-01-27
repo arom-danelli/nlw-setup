ceac9c90ace5219f6d0a2236732e1e249c0093a3
Arom Danelli <aromdanelli@hotmail.com>
2023-01-27 00:42:57 -0300

    Um script para novas fucionalidades

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector("form")
+const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ["01-01", "01-02", "01-03", "01-06"],
+}
+nlwSetup.setData(data)
+nlwSetup.load()
